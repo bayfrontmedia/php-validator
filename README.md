0179e9bc1d624b5f4d0ba99cc3a30a4379f86416
John Robinson <john@bayfrontmedia.com>
2020-07-27 16:20:35 -0400

    Initial release

---
+++ README.md
@@ -0,0 +1,574 @@
+## PHP validator
+
+Simple class used to validate types and values.
+
+- [License](#license)
+- [Author](#author)
+- [Requirements](#requirements)
+- [Installation](#installation)
+- [Usage](#usage)
+
+## License
+
+This project is open source and available under the [MIT License](https://github.com/bayfrontmedia/php-array-helpers/blob/master/LICENSE).
+
+## Author
+
+John Robinson, [Bayfront Media](https://www.bayfrontmedia.com)
+
+## Requirements
+
+* PHP > 7.1.0
+
+## Installation
+
+```
+composer require bayfrontmedia/php-validator
+```
+
+## Usage
+
+**Example:**
+
+```
+use Bayfront\Validator\Validate;
+
+$string = 'This is a string.';
+
+if (!Validate::empty($string)) {
+    // Do something
+}
+```
+
+**Strings:**
+
+- [empty](#empty)
+- [matches](#matches)
+- [minLength](#minlength)
+- [maxLength](#maxlength)
+- [lengthEquals](#lengthequals)
+- [lengthBetween](#lengthbetween)
+- [contains](#contains)
+- [startsWith](#startswith)
+- [endsWith](#endswith)
+- [email](#email)
+- [url](#url)
+- [ip](#ip)
+- [ipv4](#ipv4)
+- [ipv6](#ipv6)
+- [alpha](#alpha)
+- [alphaNumeric](#alphanumeric)
+
+**Numbers:**
+
+- [lessThan](#lessthan)
+- [greaterThan](#greaterthan)
+- [lessThanOrEqual](#lessthanorequal)
+- [greaterThanOrEqual](#greaterthanorequal)
+- [equals](#equals)
+- [between](#between)
+
+**Types:**
+
+- [integer](#integer)
+- [float](#float)
+- [boolean](#boolean)
+- [object](#object)
+- [array](#array)
+- [string](#string)
+- [json](#json)
+
+<hr />
+
+### empty
+
+**Description:**
+
+Checks if string is empty.
+
+**Parameters:**
+
+- `$string` (string)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### matches
+
+**Description:**
+
+Checks if two strings match.
+
+**Parameters:**
+
+- `$string` (string)
+- `$matches` (string)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### minLength
+
+**Description:**
+
+Checks if string length is greater than or equal to a given length.
+
+**Parameters:**
+
+- `$string` (string)
+- `$length` (int)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### maxLength
+
+**Description:**
+
+Checks if string length is less than or equal to a given length.
+
+**Parameters:**
+
+- `$string` (string)
+- `$length` (int)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### lengthEquals
+
+**Description:**
+
+Checks if string length is equal to a given length.
+
+**Parameters:**
+
+- `$string` (string)
+- `$length` (int)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### lengthBetween
+
+**Description:**
+
+Checks if string length is between min and max length.
+
+**Parameters:**
+
+- `$string` (string)
+- `$min` (int)
+- `$max` (int)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### contains
+
+**Description:**
+
+Checks if string contains case-sensitive needle(s).
+
+**Parameters:**
+
+- `$string` (string)
+- `$needles` (string|array)
+
+**Returns:**
+
+- (bool)
+
+**Example:**
+
+```
+use Bayfront\Validator\Validate;
+
+$string = 'This is a string.';
+
+if (!Validate::contains($string, [
+    'This',
+    'is a'
+])) {
+    // Do something
+}
+```
+
+<hr />
+
+### startsWith
+
+**Description:**
+
+Checks if string begins with a given needle.
+
+**Parameters:**
+
+- `$string` (string)
+- `$needle` (string)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### endsWith
+
+**Description:**
+
+Checks if string ends with a given needle.
+
+**Parameters:**
+
+- `$string` (string)
+- `$needle` (string)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### email
+
+**Description:**
+
+Checks if string validates as email.
+
+**Parameters:**
+
+- `$string` (string)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### url
+
+**Description:**
+
+Checks if string validates as a URL.
+
+**Parameters:**
+
+- `$string` (string)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### ip
+
+**Description:**
+
+Checks if string validates as an IP address.
+
+**Parameters:**
+
+- `$string` (string)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### ipv4
+
+**Description:**
+
+Checks if string validates as an IPv4 address.
+
+**Parameters:**
+
+- `$string` (string)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### ipv6
+
+**Description:**
+
+Checks if string validates as an IPv6 address.
+
+**Parameters:**
+
+- `$string` (string)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### alpha
+
+**Description:**
+
+Checks if string only contains alpha characters.
+
+**Parameters:**
+
+- `$string` (string)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### alphaNumeric
+
+**Description:**
+
+Checks if string only contains alphanumeric characters.
+
+**Parameters:**
+
+- `$string` (string)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### lessThan
+
+**Description:**
+
+Checks if value is less than a given number.
+
+**Parameters:**
+
+- `$value` (int)
+- `$compare` (int)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### greaterThan
+
+**Description:**
+
+Checks if value is greater than a given number.
+
+**Parameters:**
+
+- `$value` (int)
+- `$compare` (int)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### lessThanOrEqual
+
+**Description:**
+
+Checks if value is less than or equal to a given number.
+
+**Parameters:**
+
+- `$value` (int)
+- `$compare` (int)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### greaterThanOrEqual
+
+**Description:**
+
+Checks if value is greater than or equal to a given number.
+
+**Parameters:**
+
+- `$value` (int)
+- `$compare` (int)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### equals
+
+**Description:**
+
+Checks if values are equal.
+
+**Parameters:**
+
+- `$value` (int)
+- `$compare` (int)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### between
+
+**Description:**
+
+Checks if value is between given min and max values.
+
+**Parameters:**
+
+- `$value` (int)
+- `$min` (int)
+- `$max` (int)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### integer
+
+**Description:**
+
+Checks if value is an integer.
+
+**Parameters:**
+
+- `$value` (mixed)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### float
+
+**Description:**
+
+Checks if value is a float.
+
+**Parameters:**
+
+- `$value` (mixed)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### boolean
+
+**Description:**
+
+Checks if value is a boolean.
+
+**Parameters:**
+
+- `$value` (mixed)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### object
+
+**Description:**
+
+Checks if value is an object.
+
+**Parameters:**
+
+- `$value` (mixed)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### array
+
+**Description:**
+
+Checks if value is an array.
+
+**Parameters:**
+
+- `$value` (mixed)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### string
+
+**Description:**
+
+Checks if value is a string.
+
+**Parameters:**
+
+- `$value` (mixed)
+
+**Returns:**
+
+- (bool)
+
+<hr />
+
+### json
+
+**Description:**
+
+Checks if value is a JSON string.
+
+**Parameters:**
+
+- `$value` (mixed)
+
+**Returns:**
+
+- (bool)
---
+++ src/Validate.php
@@ -0,0 +1,451 @@
+<?php
+/**
+ * Simple class used to validate a variety of data
+ *
+ * @version     1.0.0
+ * @link        https://github.com/bayfrontmedia/php-validator
+ * @license     MIT https://opensource.org/licenses/MIT
+ * @copyright   2020 Bayfront Media https://www.bayfrontmedia.com
+ * @author      John Robinson <john@bayfrontmedia.com>
+ */
+
+namespace Bayfront\Validator;
+
+class Validate
+{
+
+    /*
+     * ############################################################
+     * Strings
+     * ############################################################
+     */
+
+    /**
+     * Is string empty
+     *
+     * @param string $string
+     *
+     * @return bool
+     */
+
+    public static function empty(string $string): bool
+    {
+        return $string == '';
+    }
+
+    /**
+     * Checks if two strings match
+     *
+     * @param string $string
+     * @param string $matches
+     *
+     * @return bool
+     */
+
+    public static function matches(string $string, string $matches): bool
+    {
+        return $string == $matches;
+    }
+
+    /**
+     * Checks if string length is greater than or equal to a given length
+     *
+     * @param string $string
+     * @param int $length
+     *
+     * @return bool
+     */
+
+    public static function minLength(string $string, int $length): bool
+    {
+        return strlen($string) >= $length;
+    }
+
+    /**
+     * Checks if string length is less than or equal to a given length
+     *
+     * @param string $string
+     * @param int $length
+     *
+     * @return bool
+     */
+
+    public static function maxLength(string $string, int $length): bool
+    {
+        return strlen($string) <= $length;
+    }
+
+    /**
+     * Checks if string length is equal to a given length
+     *
+     * @param string $string
+     * @param int $length
+     *
+     * @return bool
+     */
+
+    public static function lengthEquals(string $string, int $length): bool
+    {
+        return strlen($string) == $length;
+    }
+
+    /**
+     * Checks if string length is between min and max length
+     *
+     * @param string $string
+     * @param int $min
+     * @param int $max
+     *
+     * @return bool
+     */
+
+    public static function lengthBetween(string $string, int $min, int $max): bool
+    {
+        return strlen($string) >= $min && strlen($string) <= $max;
+    }
+
+    /**
+     * Checks if string contains case-sensitive needle(s)
+     *
+     * @param string $string
+     * @param $needles string|array
+     *
+     * @return bool
+     */
+
+    public static function contains(string $string, $needles): bool
+    {
+        $needles = (array)$needles;
+
+        foreach ($needles as $needle) {
+
+            if (strpos($string, $needle) === false) {
+
+                return false;
+
+            }
+
+        }
+
+        return true;
+
+    }
+
+    /**
+     * Checks if string begins with a given needle
+     *
+     * @param string $string
+     * @param string $needle
+     *
+     * @return bool
+     */
+
+    public static function startsWith(string $string, string $needle): bool
+    {
+        return substr($string, 0, strlen($needle)) === $needle;
+    }
+
+    /**
+     * Checks if string ends with a given needle
+     *
+     * @param string $string
+     * @param string $needle
+     *
+     * @return bool
+     */
+
+    public static function endsWith(string $string, string $needle): bool
+    {
+        return substr($string, -strlen($needle)) === $needle;
+    }
+
+    /**
+     * Checks if string validates as email
+     *
+     * @param string $string
+     *
+     * @return bool
+     */
+
+    public static function email(string $string): bool
+    {
+        return filter_var($string, FILTER_VALIDATE_EMAIL);
+    }
+
+    /**
+     * Checks if string validates as a URL
+     *
+     * @param string $string
+     *
+     * @return bool
+     */
+
+    public static function url(string $string): bool
+    {
+        return filter_var($string, FILTER_VALIDATE_URL);
+    }
+
+    /**
+     * Checks if string validates as an IP address
+     *
+     * @param string $string
+     *
+     * @return bool
+     */
+
+    public static function ip(string $string): bool
+    {
+        return filter_var($string, FILTER_VALIDATE_IP);
+    }
+
+    /**
+     * Checks if string validates as an IPv4 address
+     *
+     * @param string $string
+     *
+     * @return bool
+     */
+
+    public static function ipv4(string $string): bool
+    {
+        return filter_var($string, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
+    }
+
+    /**
+     * Checks if string validates as an IPv6 address
+     *
+     * @param string $string
+     *
+     * @return bool
+     */
+
+    public static function ipv6(string $string): bool
+    {
+        return filter_var($string, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6);
+    }
+
+    /**
+     * Checks if string only contains alpha characters
+     *
+     * @param string $string
+     *
+     * @return bool
+     */
+
+    public static function alpha(string $string): bool
+    {
+        return (ctype_alpha($string)) ? true : false;
+    }
+
+    /**
+     * Checks if string only contains alphanumeric characters
+     *
+     * @param string $string
+     *
+     * @return bool
+     */
+
+    public static function alphaNumeric(string $string): bool
+    {
+        return (ctype_alnum($string)) ? true : false;
+    }
+
+    /*
+     * ############################################################
+     * Numbers
+     * ############################################################
+     */
+
+    /**
+     * Checks if value is less than a given number
+     *
+     * @param int $value
+     * @param int $compare
+     *
+     * @return bool
+     */
+
+    public static function lessThan(int $value, int $compare): bool
+    {
+        return $value < $compare;
+    }
+
+    /**
+     * Checks if value is greater than a given number
+     *
+     * @param int $value
+     * @param int $compare
+     *
+     * @return bool
+     */
+
+    public static function greaterThan(int $value, int $compare): bool
+    {
+        return $value > $compare;
+    }
+
+    /**
+     * Checks if value is less than or equal to a given number
+     *
+     * @param int $value
+     * @param int $compare
+     *
+     * @return bool
+     */
+
+    public static function lessThanOrEqual(int $value, int $compare): bool
+    {
+        return $value <= $compare;
+    }
+
+    /**
+     * Checks if value is greater than or equal to a given number
+     *
+     * @param int $value
+     * @param int $compare
+     *
+     * @return bool
+     */
+
+    public static function greaterThanOrEqual(int $value, int $compare): bool
+    {
+        return $value >= $compare;
+    }
+
+    /**
+     * Checks if values are equal
+     *
+     * @param int $value
+     * @param int $compare
+     *
+     * @return bool
+     */
+
+    public static function equals(int $value, int $compare): bool
+    {
+        return $value == $compare;
+    }
+
+    /**
+     * Checks if value is between given min and max values
+     *
+     * @param int $value
+     * @param int $min
+     * @param int $max
+     *
+     * @return bool
+     */
+
+    public static function between(int $value, int $min, int $max): bool
+    {
+        return $value >= $min && $value <= $max;
+    }
+
+    /*
+     * ############################################################
+     * Types
+     * ############################################################
+     */
+
+    /**
+     * Checks if value is an integer
+     *
+     * @param mixed $value
+     *
+     * @return bool
+     */
+
+    public static function integer($value): bool
+    {
+        return is_int($value);
+    }
+
+    /**
+     * Checks if value is a float
+     *
+     * @param mixed $value
+     *
+     * @return bool
+     */
+
+    public static function float($value): bool
+    {
+        return is_float($value);
+    }
+
+    /**
+     * Checks if value is a boolean
+     *
+     * @param mixed $value
+     *
+     * @return bool
+     */
+
+    public static function boolean($value): bool
+    {
+        return is_bool($value);
+    }
+
+    /**
+     * Checks if value is an object
+     *
+     * @param mixed $value
+     *
+     * @return bool
+     */
+
+    public static function object($value): bool
+    {
+        return is_object($value);
+    }
+
+    /**
+     * Checks if value is an array
+     *
+     * @param mixed $value
+     *
+     * @return bool
+     */
+
+    public static function array($value): bool
+    {
+        return is_array($value);
+    }
+
+    /**
+     * Checks if value is a string
+     *
+     * @param mixed $value
+     *
+     * @return bool
+     */
+
+    public static function string($value): bool
+    {
+        return is_string($value);
+    }
+
+    /**
+     * Checks if value is a JSON string
+     *
+     * @param mixed $value
+     *
+     * @return bool
+     */
+
+    public static function json($value): bool
+    {
+
+        if (self::string($value)) {
+
+            json_decode($value);
+
+            return (json_last_error() == JSON_ERROR_NONE);
+
+        }
+
+        return false;
+
+    }
+
+}
